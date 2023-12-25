77872f81c09d339c94321282e2129b6fe13734e4
Davud <davud.aliyev2007@gmail.com>
2023-12-25 13:57:02 +0400

    Add project files.

---
+++ Homework 4/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace Homework_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //Verilmiş yazıdan içində boşluqlar olmayan yeni bir yazı düzəltmək
+            //Console.WriteLine("Yazini daxil edin: ");
+            //string text=Console.ReadLine();
+
+            //string text1 = "";
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (text[i] != ' ')
+            //    {
+            //        text1 += text[i];
+            //    }
+            //}
+
+            //Console.WriteLine(text1);
+
+            #endregion
+
+            #region task2
+            //Verilmiş ədədlər siyahısındaki verilmiş n ədədinin yerləşdiyi indexi tapan proqram
+
+
+            //Console.WriteLine("Ededlerin sayini daxil edin: ");
+            //string numStr = Console.ReadLine();
+            //int num=Convert.ToInt32(numStr);
+
+            //int[] num1= new int[num];
+
+            //string output = "";
+            //for (int i = 0; i < num1.Length; i++)
+            //{
+            //    Console.WriteLine($"{i + 1}. ededi yaz");
+            //    string num2Str = Console.ReadLine();
+            //    int num2 = Convert.ToInt32(numStr);
+            //    num1[i] = num;
+            //}
+
+            //Console.WriteLine("Indexi tapmaq istediyiniz ededi daxil edin: ");
+            //int target = int.Parse(Console.ReadLine());
+
+            //int index = -1;
+
+            //for (int i = 0;i < num1.Length;i++)
+            //{
+            //    if (num1[i] == target)
+            //    {
+            //        index = i;
+            //        break;
+            //    }
+            //}
+
+            //Console.WriteLine($"{target} indexi {index}-e beraberdir");
+
+
+
+
+
+            #endregion
+
+            #region task3
+            // Verilmiş yazılar siyahısında verilmiş hərfdən neçə ədəd olduğunu tapan proqram
+
+
+            //Console.WriteLine("Yazini daxil edin:");
+            //string text = Console.ReadLine();
+
+            //Console.WriteLine("Herfi daxil edin: ");
+            //string herf = Console.ReadLine();
+            //var text2 = stringValue.ToCharArray();
+
+            //int count = 0;
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (text[i] = herf)
+            //        count++;
+            //}
+
+            //Console.WriteLine($"Yazida {count} sayda {herf} herfi var");
+
+
+            #endregion
+
+            #region task4
+            //Verilmiş yazının əvvəlindəki boşluqlar silinmiş bir string düzəldən proqram
+
+            //Console.WriteLine("Yazinin Daxil edin :");
+            //string text = Console.ReadLine();
+            //string result = "";
+
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if(result != ' ')
+            //    result += text[i];
+            //}
+
+            //Console.WriteLine(result);
+
+
+            #endregion
+        }
+    }
+}
